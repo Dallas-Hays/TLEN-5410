069ea477a496bde7d7b3202a9825ade58f59b09a
Dallas <dallas.hays@colorado.edu>
2015-04-24 16:01:58 -0600

    Removed unnecessary variable

--- labs/lab6/lab6_main.py
+++ labs/lab6/lab6_main.py
@@ -14,67 +14,6 @@ import xml.etree.ElementTree as etree
 import xml.parsers.expat
 from lab6_connection import Lab6
 
-first_config = """<rpc-reply><configuration>
-    <version>9.3R4.4</version>
-    <system>
-        <host-name>rancid</host-name>
-        <login>
-            <user>
-                <name>admin</name>
-                <full-name>Administrator</full-name>
-                <uid>2001</uid>
-                <class>superuser</class>
-            </user>
-            <user>
-                <name>netman</name>
-                <uid>2000</uid>
-                <class>super-user</class>
-            </user>
-
-        </login>
-        <services>
-            <ssh>
-            </ssh>
-            <netconf>
-                <ssh>
-                </ssh>
-            </netconf>
-
-        </services>
-    </system>
-    <interfaces>
-        <interface>
-            <name>ge-0/0/0</name>
-            <unit>
-                <name>0</name>
-                <family>
-                    <inet>
-                    </inet>
-                </family>
-            </unit>
-        </interface>
-        <interface>
-            <name>ge-0/0/1</name>
-            <unit>
-                <name>0</name>
-                <mtu>1500</mtu>
-                <family>
-                    <inet>
-                    </inet>
-                </family>
-            </unit>
-        </interface>
-    </interfaces>
-    <snmp>
-        <community>
-            <name>euFohca7na</name>
-            <authorization>read-write</authorization>
-        </community>
-    </snmp>
-</configuration></rpc-reply>"""
-
-
-
 def check_config(hostname, host):
     print "Scanning the configuration for " + hostname + " ...",
 
